ec78ed0d67b2fc22ac60f70675bb347c30deeb32
eStrau <ellestrauss01@gmail.com>
2023-02-21 20:28:43 -0500

    Workshop #2 Changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className = "navbar">
+        <div className = "links"> 
+            <Link to="/home">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/experience">Experience</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
