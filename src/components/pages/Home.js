936424632b9c0569a92cc957a79d01f2d06c267c
eStrau <ellestrauss01@gmail.com>
2023-05-09 22:23:08 -0400

    updates

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
 import "./Home.css"
-
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
+
+// every time you want to push bc of updates make sure to do npm run deploy then push to github
 
 const Home = () => {
   return (
